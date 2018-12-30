aa88d8781bcc9e1698af57cbc496882f60627aa2
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-12-30 21:52:49 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,9 +2,10 @@ class Person
 
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(arg)
-    arg.each {
-    
+  def initialize(propertie)
+    properties.each { 
+      |key, value|  
+    }
   end
 
 end
